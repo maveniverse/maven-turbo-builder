233a8e6b60a0dbf8bdc912ceef10611452e03220
seregamorph <serega.morph@gmail.com>
2025-06-07 12:50:46 +0200

    Self-diagnostic for test-jar artifacts

--- maven-turbo-builder/src/main/java/com/github/seregamorph/maven/turbo/TurboMavenLifecycleParticipant.java
+++ maven-turbo-builder/src/main/java/com/github/seregamorph/maven/turbo/TurboMavenLifecycleParticipant.java
@@ -1,9 +1,15 @@
 package com.github.seregamorph.maven.turbo;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Named;
 import org.apache.maven.AbstractMavenLifecycleParticipant;
+import org.apache.maven.MavenExecutionException;
 import org.apache.maven.SessionScoped;
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
+import org.apache.maven.project.MavenProject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,25 +28,55 @@ public class TurboMavenLifecycleParticipant extends AbstractMavenLifecyclePartic
     private static final Logger logger = LoggerFactory.getLogger(TurboMavenLifecycleParticipant.class);
 
     @Override
-    public void afterProjectsRead(MavenSession session) {
-        checkBuilderAndPhase(session);
+    public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
+        if (isIsTurboBuilder(session)) {
+            checkTestJarArtifacts(session);
+            checkBuilderAndPhase(session);
+        }
     }
 
     @Override
     public void afterSessionEnd(MavenSession session) {
-        checkBuilderAndPhase(session);
+        if (isIsTurboBuilder(session)) {
+            checkBuilderAndPhase(session);
+        }
+    }
+
+    private static void checkTestJarArtifacts(MavenSession session) throws MavenExecutionException {
+        // test-jar is not supported, because package phase is now executed before compiling tests
+        for (MavenProject project : session.getProjects()) {
+            List<Plugin> jarPlugins = project.getBuildPlugins().stream()
+                .filter(plugin ->
+                    "org.apache.maven.plugins".equals(plugin.getGroupId())
+                        && "maven-jar-plugin".equals(plugin.getArtifactId()))
+                .collect(Collectors.toList());
+            for (Plugin jarPlugin : jarPlugins) {
+                for (PluginExecution pluginExecution : jarPlugin.getExecutions()) {
+                    if (pluginExecution.getGoals().contains("test-jar")) {
+                        throw new MavenExecutionException("Maven started with turbo builder (`-b turbo` CLI "
+                            + "parameter or `-bturbo` in .mvn/maven.config) and it's not compatible with " + project
+                            + " test-jar project artifacts and dependencies because of build phase reordering "
+                            + "(package phase is now executed before compiling tests). The maven-jar-plugin "
+                            + "configuration of the project has configured `test-jar` goal.",
+                            project.getFile());
+                    }
+                }
+            }
+        }
     }
 
     private static void checkBuilderAndPhase(MavenSession session) {
-        String builderId = session.getRequest().getBuilderId();
-        if (TurboBuilder.BUILDER_TURBO.equals(builderId)) {
-            if (session.getRequest().getGoals().contains("package")) {
-                logger.warn("package phase is requested in combination with turbo builder (`-bturbo` parameter \n"
-                    + "in the command line or .mvn/maven.config). Please note, that\n"
-                    + ANSI_RED + "compiling and running tests is not included in the execution" + ANSI_RESET + "\n"
-                    + "because of phase reordering.\n"
-                    + "To run tests, use `test`, `verify` or `install` phase instead of `package`.");
-            }
+        if (session.getRequest().getGoals().contains("package")) {
+            logger.warn("package phase is requested in combination with turbo builder (`-bturbo` parameter \n"
+                + "in the command line or .mvn/maven.config). Please note, that\n"
+                + ANSI_RED + "compiling and running tests is not included in the execution" + ANSI_RESET + "\n"
+                + "because of phase reordering.\n"
+                + "To run tests, use `test`, `verify` or `install` phase instead of `package`.");
         }
+    }
+
+    private static boolean isIsTurboBuilder(MavenSession session) {
+        String builderId = session.getRequest().getBuilderId();
+        return TurboBuilder.BUILDER_TURBO.equals(builderId);
     }
 }
